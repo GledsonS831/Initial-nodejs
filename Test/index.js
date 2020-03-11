9016749ebcf58c3aca7766ac7e73cc3b964dca57
GLEDSONS831 <glegogle84@gmail.com>
2020-03-11 08:16:57 -0300

    :sparkles: feat: add html

--- Test/index.js
+++ Test/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-    res.send("sou burro");
+    res.sendFile(__dirname + "/html/Test CSS.html");
 });
 
 app.get('/escola/:instituicao/:professor/:disciplina/', function(req, res){
