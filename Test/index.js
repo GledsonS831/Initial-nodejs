552c01b82abeb23ae15f84eabc516ba8b4791be7
GledsonS831 <glegogle84@gmail.com>
2020-04-02 00:27:20 -0300

    feat: :pencil: add styles.css

--- Test/index.js
+++ Test/index.js
@@ -3,12 +3,5 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + "/html/Test CSS.html");
-});
-
-app.get('/escola/:instituicao/:professor/:disciplina/', function(req, res){
-    res.send('./home/gledson/Documentos/Test/html/test1.html');
-})
-app.listen(3000, function(){
-    console.log("Server it's running");
+    res.json(__dirname + "/html/page.html");
 });
